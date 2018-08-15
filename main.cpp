acc20b36d5f07683cce31df8e4498532d93bc425
Jacyara <jacyarabosse@gmail.com>
2018-08-15 16:17:00 -0300

    LCG done

--- main.cpp
+++ main.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 #include <math.h>
 
+#define m 2147483648    // Parameters used by GCC, as seen in:
+#define a 1103515245    // https://en.wikipedia.org/wiki/Linear_congruential_generator
+#define c 12345
+
 using namespace std;
-
-#define m 2147483648
-#define a 214013
-#define c 2531011
-
-// https://en.wikipedia.org/wiki/Linear_congruential_generator
-// https://www.sanfoundry.com/cpp-program-implement-linear-congruential-generator-pseudo-random-number-generation/
 
 int main(int argc, char* argv[])
 {
-    unsigned long randomNumber = a * m;
-    randomNumber = randomNumber + c;
-    randomNumber = randomNumber % m;
+    unsigned long randomNumber;
+    unsigned long seed = 0;
 
-    unsigned long temp = randomNumber >> 16;
- 
-    cout << temp << endl;
-
-    // for (int x = 0; x < 10; x++)
-    //     cout << ms_rnd.rnd() << endl;
+    for (int x = 0; x < 10; x++) {
+        randomNumber = (a * seed + c) % m;
+        seed = randomNumber;
+        cout << (randomNumber) << endl;
+    }
  
     return 0;
 }
